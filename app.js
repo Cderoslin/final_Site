a7bb8e2c269b034bdf79fd24fa994a84cd361a46
Cderoslin <cderoslin@hotmail.com>
2022-05-10 17:33:56 -0400

    latest

--- app.js
+++ app.js
@@ -33,6 +33,12 @@ function Transitions (){
         }
     })
 
+    //Toggle
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode')
+     })
 }
 
 Transitions();
